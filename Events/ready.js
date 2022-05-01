53b59b734ecfe69255fe7fb6259c9f174e78c90f
UtsavSaini1 <utsavutsav2511@gmail.com>
2022-05-01 16:58:46 +0530

    Slick

---
+++ Events/ready.js
@@ -0,0 +1,5 @@
+module.exports = async(client) => {
+
+    client.logger.ready(`Logged in as ${client.user.tag}!`)
+    client.user.setPresence({ activity: { name: 'for a!help', type: "WATCHING" }, status: "online" });
+}
---
+++ index.js
@@ -0,0 +1,56 @@
+const Discord = require('discord.js'),
+    fs = require('fs'),
+    mongoose = require('mongoose'),
+    util = require('util'),
+    config = require('./config.json'),
+    readdir = util.promisify(fs.readdir),
+    client = new Discord.Client({ intents: ['GUILDS', 'GUILD_MEMBERS', 'GUILD_VOICE_STATES', 'GUILD_MESSAGES', 'DIRECT_MESSAGES'] });
+
+// Adding to the client
+client.event = new Discord.Collection();
+client.commands = new Discord.Collection();
+client.logger = require("./Tools/Logger.js")
+client.config = config;
+
+client.Guild = require(`./Database/Schema/Guild.js`)
+
+
+async function init() {
+    // Load Discordjs Events
+    const eventFiles = fs.readdirSync('./Events/').filter(file => file.endsWith('.js'));
+    for (const file of eventFiles) {
+        const event = require(`./Events/${file}`);
+        const eventName = file.split(".")[0];
+        client.logger.event(`Loading event ${eventName}`)
+        client.on(eventName, event.bind(null, client));
+    }
+
+    //Load the commands
+    let folders = await readdir("./Commands/");
+    folders.forEach(direct => {
+        const commandFiles = fs.readdirSync('./Commands/' + direct + "/").filter(file => file.endsWith('.js'));
+        for (const file of commandFiles) {
+            const command = require(`./Commands/${direct}/${file}`);
+            client.commands.set(command.name, command);
+        }
+    })
+
+    // Connect to the database
+    mongoose.connect(config.mongoDB, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+    }).then(() => {
+        client.logger.log('Connected to MongoDB')
+    }).catch((err) => {
+        client.log.error('Unable to connect to MongoDB Database.\nError: ' + err)
+    })
+
+    await client.login(config.token)
+}
+
+init();
+
+process.on('unhandledRejection', err => {
+    console.log('Unknown error occured:\n')
+    console.log(err)
+})
