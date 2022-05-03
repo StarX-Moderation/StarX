6a8553fabaeccf2260280dce05af0a5ae20ef8d6
UtsavSaini1 <utsavutsav2511@gmail.com>
2022-05-03 20:54:12 +0530

    slick

--- Commands/utility/giveaway.js
+++ Commands/utility/giveaway.js
@@ -216,7 +216,7 @@ module.exports = {
                         await i.update({ embeds: [embed3], components: [rowdisabled], allowedMentions: { repliedUser: true } }).then(err => {
                             const filter2 = m => m.author.id === message.author.id
 
-                            const collector2 = message.channel.createMessageCollector({ filter2, time: 15000 });
+                            const collector2 = message.channel.createMessageCollector(filter2, {time: 15000 });
 
                             collector2.on('collect', async msgcollect => {
 
